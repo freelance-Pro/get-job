a184c4291ee630dc964c4849d45691d458291155
abrar189 <algourabrar@gmail.com>
2021-05-23 16:51:44 +0300

    done

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 'use strict';
 let AddmeObj=[];
-
+let MajorArray =['Engineering','Information Technology','Arts','Siences','Medical','Economy','Geographic','Others'];
 function AddMe(userName,Email,password,gender,major,txtSkills,userimg){
-    this.userName=userName;
-    this.Email=Email;
-    this.password=password;
-    this.gender=gender;
-    this.major=major;
-    this.txtSkills=txtSkills;
-    this.userimg=userimg;
-    AddmeObj.push(this);
+  this.userName=userName;
+  this.Email=Email;
+  this.password=password;
+  this.gender=gender;
+  this.major=major;
+  this.txtSkills=txtSkills;
+  this.userimg=userimg;
+  AddmeObj.push(this);
 
 }
 let userNameForm = document.getElementById('userName');
@@ -21,18 +21,27 @@ let txtSkillsform = document.getElementById('txt_area') ;
 let imgform = document.getElementById('browse');
 let submitform = document.getElementById('submit');
 
-submitform.addEventListener('click', FunctionClick);
-
-
-FunctionClick = function(event){
-  if(userNameForm == null || Emailform == null || passwordform == null || genderform ||  )
-  {
-      alert(sorry);
-  }
-  else
-  {
-    new AddMe( userNameForm,Emailform,passwordform ,genderform ,majorform , txtSkillsform ,imgform,submitform);
-
-  }
-
+let selectEl = document.getElementById('S_Major');
+for( let i=0 ; i<MajorArray.length ;i++){
+  let optionEl = document.createElement('option');
+  selectEl.appendChild(optionEl);
+  optionEl.textContent =MajorArray[i];
 }
+
+
+
+// submitform.addEventListener('click', FunctionClick);
+
+
+// FunctionClick = function(event){
+//   if(userNameForm == null || Emailform == null || passwordform == null || genderform )
+//   {
+//     alert(sorry);
+//   }
+//   else
+//   {
+//     new AddMe( userNameForm,Emailform,passwordform ,genderform ,majorform , txtSkillsform ,imgform,submitform);
+
+//   }
+
+// };
