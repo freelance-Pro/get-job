f315bd6a2ec3792c591a3a94945f019be51d2de0
‘samahAbujwaied’ <samah.abujweed@yahoo.com>
2021-05-25 23:46:56 +0300

    finishing all

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 'use strict';
 let AddmeObj = [];
-let User_Img = [];
+let item1 = localStorage.getItem('Addme');
+if(item1){
+  AddmeObj = JSON.parse(item1);
+}
 
-
-let MajorArray = ['Engineering', 'Information Technology', 'Arts', 'Siences', 'Medical', 'Economy', 'Geographic', 'Others'];
+let MajorArray = ['Others','Engineering', 'Information Technology', 'Arts', 'Siences', 'Medical', 'Economy', 'Geographic'];
 function AddMe(userName, Email, password, gender, major, txtSkills, userimg) {
   this.userName = userName;
   this.Email = Email;
@@ -13,12 +15,12 @@ function AddMe(userName, Email, password, gender, major, txtSkills, userimg) {
   this.txtSkills = txtSkills;
   this.userimg = userimg;
   AddmeObj.push(this);
-  User_Img.push(userimg);
+
   setting();
   
 }
 
-let submitform = document.getElementById('submit');
+let submitform = document.getElementById('form_submit');
 let selectEl = document.getElementById('S_Major');
 
 if(selectEl!==null){
@@ -29,38 +31,38 @@ if(selectEl!==null){
   }
 }
 if(submitform!==null){
-  submitform.addEventListener('click', FunctionClick);
+  submitform.addEventListener('submit', FunctionClick);
 }
 
 
 
 function FunctionClick(event) {
   event.preventDefault();
-  var UserN = document.forms["form"]["userName"].value;
-  var UserE = document.forms["form"]["email"].value;
-  var UserP = document.forms["form"]["passw"].value;
-  var UserG;
+
+  let usern = document.forms["form"]["userName"].value;
+  let usere = document.forms["form"]["email"].value;
+  let userp = document.forms["form"]["passw"].value;
+  let userg;
   
   if (document.getElementById('radiogender1').checked) {
-    UserG = document.getElementById('radiogender1').value;
+    userg = document.getElementById('radiogender1').value;
   } else if (document.getElementById('radiogender2').checked) {
-    UserG = document.getElementById('radiogender2').value;
+    userg = document.getElementById('radiogender2').value;
   }
-  var x = document.getElementById("S_Major").selectedIndex;
-  var UserM = document.getElementsByTagName("option")[x].value;
-  var UserT = document.forms["form"]["txt_area"].value;
-  var UserI = document.forms["form"]["url_txt"].value;
+  let x = document.getElementById("S_Major").selectedIndex;
+  let userm = document.getElementsByTagName("option")[x].value;
+  let usert = document.forms["form"]["txt_area"].value;
+  let useri = document.forms["form"]["url_txt"].value;
 
-  if (UserN == null || UserE == null || UserP == null || UserG == null
-    || UserM == null || UserT == null || UserI == null) {
-    alert('Sorry, There are some input empty');
-  } else {
-    new AddMe(UserN, UserE, UserP, UserG, UserM, UserT, UserI);
-  }}
+    new AddMe(usern, usere, userp, userg, userm, usert, useri); 
+    window.location.href = 'members.html';
+  
+  
+}
 
 
 function setting() {
   let data = JSON.stringify(AddmeObj);
-  localStorage.setItem('Add_Me', data);
+  localStorage.setItem('Addme', data);
 }
 
